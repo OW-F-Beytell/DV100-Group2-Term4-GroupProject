df56918a300fd0aa192a6cb7e764e1f708324333
nicolaprinsloo <231021@virtualwindow.co.za>
2023-10-16 14:12:07 +0200

    Form and Validation Log In page

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,27 @@ $(document).ready(function() {
     });
 
 });
+
+
+$(document).ready(function() {
+
+    //-------------------------------------------------------------------------------------------------------
+    //Log In Form
+    //-------------------------------------------------------------------------------------------------------
+
+    //On Submit, prevent default form submission behaviour and do this instead.
+
+    $('#loginForm').submit(function(event) {
+
+        event.preventDefault();
+
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any code that should run on submit
+            window.location.href = '../pages/homepage.html';
+        }
+        $(this).addClass('was-validated');
+    });
+
+});
