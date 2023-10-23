1d39099b90fbb5b7c50cada471ad441e14bf4547
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-10-23 15:11:44 +0200

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,34 @@
-$(document).ready(function () {
-    // Sample movie data for demonstration
-    const movies = [
-        {
-            title: "Movie 1",
-            year: 2023,
-            genre: "Action",
-            imdbScore: 8.5,
-            imageUrl: "movie1.jpg",
-        },
-        {
-            title: "Movie 2",
-            year: 2022,
-            genre: "Sci-Fi",
-            imdbScore: 9.0,
-            imageUrl: "movie2.jpg",
-        },
-        // Add more movie data here
-    ];
+// index.js
 
-    // Function to populate movie cards
-    function populateMovieCards() {
-        const movieList = $('#movie-list');
-        movieList.empty();
-
-        for (let i = 0; i < movies.length; i++) {
-            const movie = movies[i];
-
-            const movieCard = $('<div class="movie-card">');
-            const movieImage = $(`<img src="${movie.imageUrl}" alt="${movie.title}">`);
-            const movieDetails = $('<div class="movie-details">');
-
-            movieDetails.append(`<h3>${movie.title}</h3>`);
-            movieDetails.append(`<p>Year: ${movie.year}</p>`);
-            movieDetails.append(`<p>Genre: ${movie.genre}</p>`);
-            movieDetails.append(`<p>IMDB Score: ${movie.imdbScore}</p>`);
-            // Add buttons for individual movie page and watchlist
-
-            movieCard.append(movieImage);
-            movieCard.append(movieDetails);
-            movieList.append(movieCard);
-        }
+function loadScript(src, callback) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.onload = callback;
+    document.head.appendChild(script);
+  }
+  
+  // List of scripts to load dynamically
+  var scriptsToLoad = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js',
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js'
+  ];
+  
+  // Load the scripts one by one
+  function loadScriptsSequentially(scripts, index) {
+    if (index < scripts.length) {
+      loadScript(scripts[index], function() {
+        // Callback function for the loaded script
+        loadScriptsSequentially(scripts, index + 1);
+      });
+    } else {
+      // All scripts are loaded
+      // You can add your code here to execute after all scripts are loaded
     }
-
-    // Sample initial movie population
-    populateMovieCards();
-
-    // Add event listeners for filter options and update the movie list
-    $('#genre, #year, #imdb-score').change(function () {
-        // Implement filtering logic based on user selections
-        // Update the movies array with filtered data
-        // Call populateMovieCards() to display the filtered movies
-    });
-});
+  }
+  
+  // Start loading scripts sequentially
+  loadScriptsSequentially(scriptsToLoad, 0);
+  
