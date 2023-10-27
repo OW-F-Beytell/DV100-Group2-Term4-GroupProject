0acbf9df4a5c8f2985451e1ec1bd9d0b1ceb3a64
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-10-27 09:38:25 +0200

    Main code done

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,102 @@
+$(document).ready(function() {
 
-/* Movie Library JS */
-    // script.js
+    //-------------------------------------------------------------------------------------------------------
+    //Sign Up Form
+    //-------------------------------------------------------------------------------------------------------
 
-// Define your API keys here (consider using environment variables)
-const apiKey = 'your_api_key';
+    //On Submit, prevent default form submission behaviour and do this instead.
 
-// Fetch and populate movie data
-function fetchAndPopulateMovies() {
-    // Get selected filter criteria
-    const selectedGenre = document.getElementById('genre').value;
-    const selectedYear = document.getElementById('year').value;
-    const minIMDB = document.getElementById('imdb').value;
-    const maxIMDB = document.getElementById('imdb').value;
+    $('#signupForm').submit(function(event) {
 
-    // Construct your API endpoint with API key and filter criteria
-    const apiUrl = `https://api.example.com/movies?api_key=${apiKey}&genre=${selectedGenre}&year=${selectedYear}&minIMDB=${minIMDB}&maxIMDB=${maxIMDB}`;
+        event.preventDefault();
 
-    // Make an API request to fetch movie data
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Clear existing movie cards
-            const movieContainer = document.getElementById('movie-container');
-            movieContainer.innerHTML = '';
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any code that should run on submit
+            window.location.href = '../pages/homepage.html';
+        }
+        $(this).addClass('was-validated');
+    });
 
-            // Populate movie cards with fetched data
-            data.forEach(movie => {
-                const card = document.createElement('div');
-                card.classList.add('col-md-3', 'mb-3');
-                card.innerHTML = `
-                    <div class="card">
-                        <img src="${movie.imageUrl}" class="card-img-top" alt="${movie.title}">
-                        <div class="card-body">
-                            <h5 class="card-title">${movie.title}</h5>
-                            <p class="card-text">${movie.description}</p>
-                            <p class="card-text">IMDB Score: ${movie.imdbScore}</p>
-                            <a href="individual-movie.html" class="btn btn-primary">View Movie</a>
-                            <button class="btn btn-secondary">Watch Movie</button>
-                        </div>
-                    </div>
-                `;
-                movieContainer.appendChild(card);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching movie data:', error);
-        });
+
+    //-------------------------------------------------------------------------------------------------------
+    //Log In Form
+    //-------------------------------------------------------------------------------------------------------
+
+    $('#loginForm').submit(function(event) {
+
+        event.preventDefault();
+
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any code that should run on submit
+            window.location.href = '../pages/homepage.html';
+        }
+        $(this).addClass('was-validated');
+    });
+
+});
+
+const form = document.querySelector('#signUpForm');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirmPassword');
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    validateForm();
+
+});
+
+function validateForm() {
+    //username
+    if(usernameInput.ariaValueMax.trim() == ''){
+        setError(usernameInput, "Name is required");
+    } else if (usernameInput.trim().length < 5){
+        setError(usernameInput, "Name needs to contain a minimum of 3 characters");
+    } else {
+        setSuccess(usernameInput);
+    }
+
+    //email
+    if(emailInput.ariaValueMax.trim() == ''){
+        setError(emailInput, "Email address is required");
+    } else if (emailInput.trim().value){
+        setError(emailInput, "Provide a valid email address");
+    } else {
+        setSuccess(emailInput);
+    }
+
+    //password
+    if(passwordInput.ariaValueMax.trim() == ''){
+        setError(passwordInput, "Enter a password");
+    } else if (passwordInput.trim().length < 8){
+        setError(passwordInput, "Password must contain at least 8 characters");
+    } else {
+        setSuccess(passwordInput);
+    }
+
+    //confirm password
+    if(confirmPasswordInput.ariaValueMax.trim() == ''){
+        setError(confirmPasswordInput, "Enter a password");
+    } else if (confirmPasswordInput.trim().value !== passwordInput.value){
+        setError(confirmPasswordInput, "Password must contain at least 8 characters");
+    } else {
+        setSuccess(confirmPasswordInput);
+    }
+
 }
 
-// Add event listeners to trigger movie fetch when filter criteria change
-document.getElementById('genre').addEventListener('change', fetchAndPopulateMovies);
-document.getElementById('year').addEventListener('change', fetchAndPopulateMovies);
-document.getElementById('imdb').addEventListener('change', fetchAndPopulateMovies);
 
-// Initial population of movies on page load
-fetchAndPopulateMovies();
+// Local Storage
+let validateForm_serialized = JSON.stringify(validateForm);
+
+localStorage.setItem("validateForm", validateForm_serialized);
+
+let validateForm_deserialized = JSON.parse(localStorage.getItem("validatedForm"));
+
+console.log("validateForm", validateForm_deserialized);
