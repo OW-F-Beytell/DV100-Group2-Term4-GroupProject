6feb3a76a2abe0b98affe66dd8236213df4cdf91
Francois56789 <126235118+Franna2@users.noreply.github.com>
2023-11-03 09:11:55 +0200

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,157 +1,102 @@
-// JavaScript code in your library.js file
+$(document).ready(function() {
 
-// Define a function to populate movie cards
-function populateMovieCards(movieData) {
-    const movieContainer = document.querySelector('.movies');
-  
-    // Iterate through the movie data and create a card for each movie
-    movieData.forEach((movie) => {
-      const movieCard = document.querySelector('.movie').cloneNode(true);
-  
-      // Populate the card with movie data
-      const cardTitle = movieCard.querySelector('.card-title');
-      const cardText = movieCard.querySelector('.card-text');
-      const watchNowButton = movieCard.querySelector('.btn-watch');
-  
-      cardTitle.textContent = movie.title;
-      cardText.textContent = movie.description;
-      watchNowButton.href = movie.link;
-  
-      // Append the movie card to the movies container
-      movieContainer.appendChild(movieCard);
+    //-------------------------------------------------------------------------------------------------------
+    //Sign Up Form
+    //-------------------------------------------------------------------------------------------------------
+
+    //On Submit, prevent default form submission behaviour and do this instead.
+
+    $('#signupForm').submit(function(event) {
+
+        event.preventDefault();
+
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any code that should run on submit
+            window.location.href = '../pages/homepage.html';
+        }
+        $(this).addClass('was-validated');
     });
-  }
-  
-  // Example movie data (replace with your actual data)
-  const moviecard = [
-    {
-      title: 'Movie Title 1',
-      description: 'Description for Movie 1',
-      link: 'https://example.com/movie1',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    {
-      title: 'Movie Title 2',
-      description: 'Description for Movie 2',
-      link: 'https://example.com/movie2',
-    },
-    // Add more movie objects as needed
-  ];
-  
-  // Call the populateMovieCards function with your movie data
-  populateMovieCards(movieData);
-  
+
+
+    //-------------------------------------------------------------------------------------------------------
+    //Log In Form
+    //-------------------------------------------------------------------------------------------------------
+
+    $('#loginForm').submit(function(event) {
+
+        event.preventDefault();
+
+        if (this.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            // Add any code that should run on submit
+            window.location.href = '../pages/homepage.html';
+        }
+        $(this).addClass('was-validated');
+    });
+
+});
+
+const form = document.querySelector('#signUpForm');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirmPassword');
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    validateForm();
+
+});
+
+function validateForm() {
+    //username
+    if(usernameInput.ariaValueMax.trim() == ''){
+        setError(usernameInput, "Name is required");
+    } else if (usernameInput.trim().length < 5){
+        setError(usernameInput, "Name needs to contain a minimum of 3 characters");
+    } else {
+        setSuccess(usernameInput);
+    }
+
+    //email
+    if(emailInput.ariaValueMax.trim() == ''){
+        setError(emailInput, "Email address is required");
+    } else if (emailInput.trim().value){
+        setError(emailInput, "Provide a valid email address");
+    } else {
+        setSuccess(emailInput);
+    }
+
+    //password
+    if(passwordInput.ariaValueMax.trim() == ''){
+        setError(passwordInput, "Enter a password");
+    } else if (passwordInput.trim().length < 8){
+        setError(passwordInput, "Password must contain at least 8 characters");
+    } else {
+        setSuccess(passwordInput);
+    }
+
+    //confirm password
+    if(confirmPasswordInput.ariaValueMax.trim() == ''){
+        setError(confirmPasswordInput, "Enter a password");
+    } else if (confirmPasswordInput.trim().value !== passwordInput.value){
+        setError(confirmPasswordInput, "Password must contain at least 8 characters");
+    } else {
+        setSuccess(confirmPasswordInput);
+    }
+
+}
+
+
+// Local Storage
+let validateForm_serialized = JSON.stringify(validateForm);
+
+localStorage.setItem("validateForm", validateForm_serialized);
+
+let validateForm_deserialized = JSON.parse(localStorage.getItem("validatedForm"));
+
+console.log("validateForm", validateForm_deserialized);
