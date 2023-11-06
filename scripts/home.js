edaafc8a23725cfc8ddeb8027b047ff29a340800
nicolaprinsloo <231021@virtualwindow.co.za>
2023-11-06 10:03:40 +0200

    API Slider code

--- scripts/home.js
+++ scripts/home.js
@@ -9,7 +9,7 @@ function loadWatchList() {
 }
 
 const apiKey = '453832e297403c7f70c5984dbfa5ebc9';
-const tmdbEndpoint = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1&sort_by=popularity.desc`;
+const movieDetailsURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1&sort_by=popularity.desc`;
 
 const movieContainer = $('#movieContainer');
 
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
 function loadWelcomeMovieContent() {
     $.ajax({
-        url: tmdbEndpoint,
+        url: movieDetailsURL,
         method: 'GET',
         success: function (data) {
             const movies = data.results.slice(0, 12); // Load only 12 movies
@@ -89,33 +89,35 @@ function createMovieCard(movie) {
     return returnValue;
 }
 
-// Function to create a carousel item from movie data
+//Function to create a carousel item from movie data
 function createCarouselItem(movie) {
-    const director = movie.director ? movie.director : "N/A";
-    const rating = movie.vote_average ? movie.vote_average : "N/A";
+    const director = movie.movieDirector ? movie.movieDirector : "N/A";
+    const rating = movie.movieScore ? movie.movieScore : "N/A";
 
     return `
-        <div class="carousel-item">
-            <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" class="d-block w-100 background-img" alt="...">
-            <div class="overlay">
-                <div class="card m-5" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h2 class="card-title">${movie.title}</h2>
-                                <p class="card-text">Director: ${director}</p>
-                                <p>Rating: ${rating}</p>
-                                <img src="assets/Retro-btn.svg">
-                                <img src="assets/Add-btn.svg">
-                            </div>
+    <div class="carousel-item" id="carouselExampleAutoplaying" data-bs-ride="carousel">
+        <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" class="d-block w-100 background-img" alt="...">
+        <div class="overlay">
+            <div class="card m-5" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-fluid rounded-start" alt="...">
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h2 class="card-title">${movie.movieTitle}</h2>
+                            <p class="card-text">Director: ${director}</p>
+                            <p>Rating: ${rating}</p>
+                            <img src="assets/Retro-btn.svg">
+                            <img src="assets/Add-btn.svg">
                         </div>
                     </div>
                 </div>
             </div>
-        </div>`;
+        </div>
+    </div>
+    `;
 }
 
 function addToWatchList(movieID){
